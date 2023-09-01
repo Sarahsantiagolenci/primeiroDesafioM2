d9b751b3ff9cc7ff9b535e8d0930ef9679a67baf
Sarah <sarahsantiagolenci@gmail.com>
2023-09-01 22:32:25 +0100

    alteração

--- script.js
+++ script.js
@@ -1,65 +1,79 @@
 let dia= "s"
-if(tempo == "s") {
+if(dia === "s") {
     console.log("claro")
 } else {
     console.log("de noite")
 }
 
 
-for (let contador =0; contador <20; contador +=2) {
-    console.log (contador)
+for (let contador = 0; contador <= 20; contador += 2) {
+    console.log(contador)
 }
 
 
-function mensagem (frase) {
-    console.log (`a mensagem é ${frase}`);
+function mensagem(frase) {
+    console.log(`a mensagem é ${frase}`);
 }
-mensagem ("sorria, você está sendo filmado");
+mensagem("sorria, você está sendo filmado");
 
 
-function conhecer (nome) {
-    console.log (`meu nome é ${nome}`);
+function conhecer(nome) {
+    console.log(`meu nome é ${nome}`);
 }
-conhecer ("Sarah");
+conhecer("Sarah");
 
 
-function apresentar (nome, idade, estilo) {
-    console.log (`meu nome, idade, estilo musical é ${nome, idade, estilo}`);
+function apresentar(nome, idade, estilo) {
+    console.log(`meu nome: ${nome}, idade: ${idade}, estilo musical: ${estilo}`);
 }
-apresentar ("Sarah", "27 anos", "música internacional");
+apresentar("Sarah", "27 anos", "música internacional");
 
 
-function timeOff (filme, música) {
-    console.log (`um filme, uma música ${filme, música}`);
+function timeOff(filme, musica) {
+    console.log(`um filme ${filme}, uma música ${musica}`);
 }
-timeOff ("titanic", "dogDaysAreOver");
+timeOff("titanic", "dogDaysAreOver");
 
 
-7.
+function numeroTriplo(numero) {
+    let resultado = numero * 3;
+    return resultado; 
+}
+numeroTriplo(7);
 
 
-function 
+function verificadoraBool(variavel) {
+    if(variavel === true) {
+        console.log("true")
+    } else {
+        console.log("false")
+    }
+}
+let noite = true 
+dia = false 
+verificadoraBool(noite);
+verificadoraBool(dia);
 
 
 let casa = ["panela", "prato", "mesa", "tapete", "banco"];
-console.log (cozinha);
+console.log(casa);
 
-casa.unshift ("colher");
+casa.unshift("colher");
 
-casa.pop ();
+casa.pop();
 
-casa.push ("cadeira", "máquina");
+casa.push("cadeira", "máquina");
 
-casa.shift ();
+casa.shift();
 
 
 let numbers = [7,5,6,3,8,9,2,1,4];
-console.log (numeros.sort (function (a, b) {return a - b}));
+console.log (numbers.sort (function (a, b) {return a - b}));
 
 
 let mim = {
-    nascida: "Bauru, interior de São Paulo"
-    cresceu: "Botucatu, também interior"
+    nascida: "Bauru, interior de São Paulo",
+    cresceu: "Botucatu, também interior",
     formou: "Minas Gerais"
 }
 
@@ -72,39 +86,47 @@ console.log(mim);
 
 let cadastro = [
     {
-        nome: "Julia"
-        idade: 20
-        telefone: 963 852 741
+        nome: "Julia",
+        idade: 20,
+        telefone: 963852741,
         amigos: "Barnabé, Bernardo, Beto, Bruno"
     },
     {
-        nome: "Joaquina"
-        idade: 50
-        telefone: 936 825 714
+        nome: "Joaquina",
+        idade: 50,
+        telefone: 936825714,
         amigos: "Silvio, Tereza, Amaro, Maria"
     },
     {
-        nome: "Adriano"
-        idade: 55
-        telefone: 951 753 842
+        nome: "Adriano",
+        idade: 55,
+        telefone: 951753842,
         amigos: "Margarida, Jessica, Miguel, Helena"
     },
     {
-        nome: "Bruna" 
-        idade: 25
-        telefone: 987 654 321
+        nome: "Bruna", 
+        idade: 25,
+        telefone: 987654321,
         amigos: "João, André, Silvana, Rosalina"
     },
     {
-        nome: "Mário"
-        idade: 47
-        telefone: 978 645 312
+        nome: "Mário",
+        idade: 47,
+        telefone: 978645312,
         amigos: "Clarice, Valentim, Liz, Samira"
     },
 ];
 
-console.log (cadastro[1].amigos);
-e o restante?
+console.log(cadastro[0].amigos.split(`, `)[3]);
+
+console.log(cadastro[1].amigos.split(`, `)[2]);
+
+console.log(cadastro[2].amigos.split(`, `)[1]);
+
+console.log(cadastro[3].amigos.split(`, `)[0]);
+
+console.log(cadastro[4].amigos.split(`, `)[2]);
+
 
 
 
